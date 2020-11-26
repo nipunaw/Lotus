e19af8ed6df082ae48c3e4c75489ed9cc27a7d75
Nipuna Weerapperuma <nipunaw12@gmail.com>
2020-11-26 17:01:11 -0500

    Floating widget resizing update
    Overhauled resizing for a more comprehensive (side vs corner) approach. Much more readable.
    
    Remaining:
    - Slight UI adjustments
    - Cursor appearance

--- src/lotusFloating.py
+++ src/lotusFloating.py
@@ -19,44 +19,83 @@ class FloatingWidget(QtWidgets.QWidget):
 
         self.position_x = self.pos().x()
         self.position_y = self.pos().y()
-        self.setMinimumWidth(self.child_widget.width())
-        self.setMinimumHeight(self.child_widget.height())
-        self.setGeometry(50, 50, self.child_widget.width(), self.child_widget.height())
+        self.setMinimumWidth(self.child_widget.width()+6)
+        self.setMinimumHeight(self.child_widget.height()+6)
+        self.setGeometry(50, 50, self.child_widget.width()+6, self.child_widget.height()+6)
 
         self.setAttribute(QtCore.Qt.WA_StyledBackground, True)
+        self.child_widget.move(3, 3)
+        self.child_widget.setStyleSheet('border-width: 0px 0px 0px 0px;')
         self.setStyleSheet('background-color: white; border:1px solid black;')
-        self.mouse_move = False
-        self.mouse_resize = False
+
+        self.move_only = False
+        self.left_side = False
+        self.right_side = False
+        self.top_side = False
+        self.bottom_side = False
 
     def mousePressEvent(self, event: QtGui.QMouseEvent) -> None:
         if event.button() == Qt.LeftButton:
-            if self.width() - event.pos().x() < 10 or self.height() - event.pos().y() < 10:
-                self.mouse_resize = True
-            elif event.pos().x() < 10 or event.pos().y() < 10:
-                self.mouse_resize = True
-                self.mouse_move = True
-            else:
-                self.mouse_move = True
-        self.move_position = QtCore.QPoint(event.globalX() - self.geometry().x(), event.globalY() - self.geometry().y())
-        self.resize_lr_position = QtCore.QPoint(event.globalX() - self.width(), event.globalY() - self.height())
-        self.resize_ur_position = QtCore.QPoint(event.globalX() + self.width(), event.globalY() + self.height())
+            if event.pos().x() < 10:
+                self.left_side = True
+            if event.pos().y() < 10:
+                self.top_side = True
+            if self.width() - event.pos().x() < 10:
+                self.right_side = True
+            if self.height() - event.pos().y() < 10:
+                self.bottom_side = True
+            if not (self.left_side or self.top_side or self.right_side or self.bottom_side):
+                self.move_only = True
+
+        self.start_size = QtCore.QPoint(self.width(), self.height())
+        self.start_geometry = QtCore.QPoint(self.geometry().x(), self.geometry().y())
+        self.start_mouse_position = event.globalPos()
+        # self.resize_lr_position = QtCore.QPoint(event.globalX() - self.width(), event.globalY() - self.height())
+        # self.resize_cl_position = QtCore.QPoint(event.globalX() + self.width(), event.globalY() - self.height())
+        # self.resize_ur_position = QtCore.QPoint(event.globalX() + self.width(), event.globalY() + self.height())
 
     def mouseMoveEvent(self, event: QtGui.QMouseEvent) -> None:
-        if self.mouse_resize and self.mouse_move:
-            end_position = self.resize_ur_position - event.globalPos()
-            self.resize(end_position.x(), end_position.y())
-            end_position = event.globalPos() - self.move_position
-            self.move(end_position)
-        elif self.mouse_move:
-            end_position = event.globalPos() - self.move_position
-            self.move(end_position)
-        elif self.mouse_resize:
-            end_position = event.globalPos() - self.resize_lr_position # QGlobal - GGlobal + width
-            self.resize(end_position.x(), end_position.y())
+        if self.move_only:
+            self.move(event.globalPos() - self.start_mouse_position + self.start_geometry)
+        elif self.left_side and self.top_side: # upper left corner
+            resize_point = self.start_mouse_position - event.globalPos() + self.start_size
+            self.resize(resize_point.x(), resize_point.y())
+            self.move(event.globalPos() - self.start_mouse_position + self.start_geometry)
+        elif self.right_side and self.top_side: # upper right corner
+            resize_point = self.start_mouse_position - event.globalPos() + self.start_size
+            resize_point_inverted = event.globalPos() - self.start_mouse_position + self.start_size
+            self.resize(resize_point_inverted.x(), resize_point.y())
+            self.move(self.start_geometry.x(), event.globalY() - self.start_mouse_position.y() + self.start_geometry.y())
+        elif self.left_side and self.bottom_side: # lower left corner
+            resize_point = self.start_mouse_position - event.globalPos() + self.start_size
+            resize_point_inverted = event.globalPos() - self.start_mouse_position + self.start_size
+            self.resize(resize_point.x(), resize_point_inverted.y())
+            self.move(event.globalX() - self.start_mouse_position.x() + self.start_geometry.x(), self.start_geometry.y())
+        elif self.right_side and self.bottom_side: # lower right corner
+            resize_point_inverted = event.globalPos() - self.start_mouse_position + self.start_size
+            self.resize(resize_point_inverted.x(), resize_point_inverted.y())
+        elif self.left_side: # left side
+            resize_point = self.start_mouse_position - event.globalPos() + self.start_size
+            self.resize(resize_point.x(), self.start_size.y())
+            self.move(event.globalX() - self.start_mouse_position.x() + self.start_geometry.x(), self.start_geometry.y())
+        elif self.right_side: # right side
+            resize_point_inverted = event.globalPos() - self.start_mouse_position + self.start_size
+            self.resize(resize_point_inverted.x(), self.start_size.y())
+        elif self.top_side: # top side
+            resize_point = self.start_mouse_position - event.globalPos() + self.start_size
+            self.resize(self.start_size.x(), resize_point.y())
+            self.move(self.start_geometry.x(), event.globalY() - self.start_mouse_position.y() + self.start_geometry.y())
+        elif self.bottom_side: # bottom side
+            resize_point_inverted = event.globalPos() - self.start_mouse_position + self.start_size
+            self.resize(self.start_size.x(), resize_point_inverted.y())
+        self.child_widget.resize(self.width() - 6, self.height() - 6)
 
     def mouseReleaseEvent(self, event: QtGui.QMouseEvent) -> None:
-        self.mouse_move = False
-        self.mouse_resize = False
+        self.move_only = False
+        self.left_side = False
+        self.right_side = False
+        self.top_side = False
+        self.bottom_side = False
 
 
 
